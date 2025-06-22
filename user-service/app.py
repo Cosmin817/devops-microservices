e34ad7516fda7cd3113c5f855aee623d0c0ff954
Cosmin817 <cosmincmc@gmail.com>
2025-06-22 15:08:24 +0300

    added k8s manifests

--- user-service/app.py
+++ user-service/app.py
@@ -7,4 +7,4 @@ def users():
     return jsonify([{"id": 1, "name": "Alice"}, {"id": 2, "name": "Bob"}])
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5002)
