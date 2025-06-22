cd3a8b223878dfa74708e38a13c89e2b0a239524
Cosmin817 <cosmincmc@gmail.com>
2025-06-22 15:30:31 +0300

    test change port

--- user-service/app.py
+++ user-service/app.py
@@ -7,4 +7,4 @@ def users():
     return jsonify([{"id": 1, "name": "Alice"}, {"id": 2, "name": "Bob"}])
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5002)
+    app.run(host="0.0.0.0", port=5003)
